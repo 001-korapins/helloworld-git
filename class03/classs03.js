0f3f423d0fb429d2b3c8e13ed055095100327640
Korapin <224613273+001-korapins@users.noreply.github.com>
2026-03-14 15:46:36 +0700

    Todo-List

---
+++ class03/classs03.js
@@ -0,0 +1,32 @@
+// let count = 1
+
+// while (count <= 6){
+//     console.log(`การรันครั้งที่ ${count}`)
+//     count++
+// }
+
+// const fruits = ["Apple", "Coconut", "Banana"];
+
+// console.log(fruits[1])
+
+// let number = 5
+// let i = 1
+
+// while (i <= 12) {
+//     console.log(`${number} x ${i} = ${number * i}`)
+//     i++
+// }
+
+//โจทย์ที่ 2 : Filter Adults
+// const users = [
+//     {name:"Alice", age:15},
+//     {name:"Bob", age:22},
+//     {name:"Charlie", age:18},
+//     {name:"David", age:12}
+// ];
+
+// let result = users.filter((i) => {
+//     return i.age > 18
+// })
+
+// console.log(result)
